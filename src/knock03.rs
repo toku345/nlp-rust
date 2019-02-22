59e1683b854020ddd5799baacdbe7c4645daa402
Fumitaka Tokumitsu <toku345@gmail.com>
2019-02-22 22:29:10 +0900

    2単語ケース追加

--- src/knock03.rs
+++ src/knock03.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 pub fn calc_word_length_map(input_string: &str) -> HashMap<&str, usize> {
-    [(input_string, input_string.len())]
-        .iter()
-        .cloned()
+    input_string
+        .split_whitespace()
+        .map(|word| (word, word.len()))
         .collect()
 }
 
@@ -19,7 +19,16 @@ mod test {
         let result = calc_word_length_map(&input_string);
 
         let expected: HashMap<&str, usize> = [("Now", 3)].iter().cloned().collect();
+        assert_eq!(result, expected);
+    }
 
+    #[test]
+    fn test_make_word_length_map_from_2_words() {
+        let input_string = String::from("Now I");
+
+        let result = calc_word_length_map(&input_string);
+
+        let expected: HashMap<&str, usize> = [("Now", 3), ("I", 1)].iter().cloned().collect();
         assert_eq!(result, expected);
     }
 }
